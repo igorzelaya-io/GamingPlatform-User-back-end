d8fa65603625ce0132f9b013ebec2a02d41d2c8d
Igor Zelaya <igorzelaya@Igors-MacBook-Pro-2.local>
2021-02-25 16:32:02 -0600

    Updated Source code

---
+++ src/main/java/com/d1gaming/user/userTournament/UserTournamentController.java
@@ -0,0 +1,36 @@
+package com.d1gaming.user.userTournament;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.d1gaming.library.tournament.Tournament;
+
+@RestController
+@CrossOrigin(origins = "localhost:4200")
+@PreAuthorize("permitAll()")
+@RequestMapping(value = "/usertournamentsapi")
+public class UserTournamentController {
+
+	@Autowired
+	private UserTournamentService userTournamentService; 
+	
+	@GetMapping(value = "/userTournaments")
+	public ResponseEntity<?> getAllTournamentsFromUser(@RequestParam(required = true)String userId) throws InterruptedException, ExecutionException{
+		List<Tournament> userTournaments = userTournamentService.getAllTournamentsFromUser(userId);
+		if(userTournaments.isEmpty()) {
+			return new ResponseEntity<>(userTournaments, HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(userTournaments, HttpStatus.OK);
+	}
+	
+}
--- src/main/java/com/d1gaming/user/userTournament/UserTournamentService.java
+++ src/main/java/com/d1gaming/user/userTournament/UserTournamentService.java
@@ -1,42 +1,93 @@
 package com.d1gaming.user.userTournament;
 
-import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.d1gaming.library.tournament.Tournament;
+import com.d1gaming.library.tournament.TournamentStatus;
+import com.d1gaming.library.user.User;
+import com.d1gaming.library.user.UserStatus;
+import com.d1gaming.library.user.UserTournament;
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.EventListener;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreException;
+import com.google.cloud.firestore.QuerySnapshot;
 
+@Service
 public class UserTournamentService {
 
 	private final String USER_TOURNAMENT_SUBCOLLECTION = "userTournaments";
 	
+	private final String USER_COLLECTION = "users";
+	
 	@Autowired
 	private Firestore firestore;
 	
-	private CollectionReference getUserTournamentsSubcollection() {
-		return firestore.collection(this.USER_TOURNAMENT_SUBCOLLECTION);
+	private CollectionReference getUsersCollection() {
+		return firestore.collection(USER_COLLECTION);
 	}
 	
-	public void updateUserKillsOnTournament(String tournamentId) {
-		DocumentReference reference = getUserTournamentsSubcollection().document(tournamentId);
-		reference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-			@Override
-			public void onEvent(@Nullable DocumentSnapshot snapshot,
-								@Nullable FirestoreException e) {
-				if(e != null) {
-					throw new RuntimeException(e.getMessage());
-				}
-				if(snapshot != null && snapshot.exists()) {
-					System.out.println("Data: " + snapshot.getData());
-				}
-			}
-		});
+	private CollectionReference getTournamentsSubcollectionFromUser(String userId) {
+		return firestore.collection(USER_COLLECTION)
+						.document(userId)
+						.collection(USER_TOURNAMENT_SUBCOLLECTION);	
 	}
 	
+	private DocumentReference getUserReference(String userId) {
+		return getUsersCollection().document(userId);
+	}
+	
+	private DocumentReference getUserTournamentReferenceFromUser(String userId, String tournamentId) {
+		return getTournamentsSubcollectionFromUser(userId).document(tournamentId);
+	}
+	
+	private DocumentReference getTournamentReferenceFromTournamentCollection(String tournamentId) {
+		return firestore.collection("tournaments").document(tournamentId);
+	}
+	
+	private boolean isActive(String userId) throws InterruptedException, ExecutionException {
+		DocumentReference userReference = getUsersCollection().document(userId);
+		DocumentSnapshot userSnapshot = userReference.get().get();
+		if(userSnapshot.exists() && userSnapshot.toObject(User.class).getUserStatusCode().equals(UserStatus.ACTIVE)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isActiveTournament(String tournamentId) throws InterruptedException, ExecutionException {
+		DocumentReference tournamentReference = firestore.collection("tournaments").document(tournamentId);
+		DocumentSnapshot tournamentSnapshot = tournamentReference.get().get();
+		if(tournamentSnapshot.exists() && tournamentSnapshot.toObject(Tournament.class).getTournamentStatus().equals(TournamentStatus.ACTIVE)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public List<Tournament> getAllTournamentsFromUser(String userId) throws InterruptedException, ExecutionException{
+		if(isActive(userId)) {
+			ApiFuture<QuerySnapshot> queryForTournaments = getTournamentsSubcollectionFromUser(userId).get();
+			return queryForTournaments.get().getDocuments()
+											.stream()
+											.map(document -> document.toObject(UserTournament.class))
+										    .map(userTournament -> userTournament.getUserTournaments())
+										    .filter( tournament -> {
+												try {
+													return isActive(tournament.getTournamentId());
+												} catch (InterruptedException | ExecutionException e) { 
+													e.printStackTrace();
+												} 
+												return false;
+											} )
+										    .collect(Collectors.toList());
+		}
+		return new ArrayList<>();
+	}
 	
 }
